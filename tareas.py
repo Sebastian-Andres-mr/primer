f598099af99f797f7620400e866379729b1b1837
Sebastian-Andres-mr <sea.miranda@duocuc.cl>
2026-06-03 18:58:09 -0400

    Add files via upload
    hola

---
+++ tareas.py
@@ -0,0 +1,38 @@
+tareas ={
+    "hogar":[],
+    "trabajo":[],
+    "estudio":[]
+    
+}
+while True:
+    print("---Gestión de tareas---")
+    print("1- Hogar")
+    print("2- Trabajo")
+    print("3- Estudio")
+    print("4- Listar tareas")
+    print("5- Salir")
+    op = input("selecciones una opción: ")
+    if op in "12345":
+        if op in "123":
+            tarea = input("Ingrese la tarea: ")
+    match op:
+        case "1":
+            tareas["hogar"].append(tarea)
+        case "2":
+            tareas["trabajo"].append(tarea)
+        case "3":
+            tareas["Estudio"].append(tarea)
+        case "4":
+            print("---Detalle de Tareas---")
+            print(f"Hogar - {len(tareas["hogar"])}")
+            for tarea in tareas["hogar"]:
+                print(f"  -{tarea}")
+            print(f"Trabajo - {len(tareas["Trabajo"])}")
+            for tarea in tareas["Trabajo"]:
+                print(f"  -{tarea}")
+            print(f"Estudio - {len(tareas["Estudio"])}")
+            for tarea in tareas["Estudio"]:
+                print(f"  -{tarea}")
+        case "5":
+                print("chaito")
+                break
